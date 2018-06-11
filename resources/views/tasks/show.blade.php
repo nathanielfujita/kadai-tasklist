24b4a95635205617b8341d6cb0a05c302a118e27
EC2 Default User <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-06-11 05:43:43 +0000

    edited mistakes

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,8 +4,8 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>タイトル: {{ $task->title }}</p>
-    <p>メッセージ: {{ $task->content }}</p>
+    <p>ステータス: {{ $task->status }}</p>
+    <p>タスク: {{ $task->content }}</p>
 
     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
 
